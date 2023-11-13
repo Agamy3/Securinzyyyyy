e6277c0ecdd6fcf123490a8bf1261b91162e1bfb
Mohamed Agamy <cds.mohamedahmed30407@alexu.edu.eg>
2023-11-13 12:41:56 +0200

    new updates

--- app/src/main/java/com/example/securinzy/MainActivity.java
+++ app/src/main/java/com/example/securinzy/MainActivity.java
@@ -2,13 +2,48 @@ package com.example.securinzy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private TextView signUpTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        emailEditText = findViewById(R.id.editTextTextEmailAddress);
+        passwordEditText = findViewById(R.id.editTextTextPassword);
+
+        signUpTextView = findViewById(R.id.textView);
+        signUpTextView.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, signup.class);
+                startActivity(intent);
+
+            }
+
+        });
+
+        Button loginButton = findViewById(R.id.button);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Get email and password
+                // Validate credentials
+                // Launch home activity on success
+            }
+        });
     }
 }
