e1b3446206c6a6c792a3d641ebf660d650d5e2bd
ChristineGuirguis <131295022+ChristineGuirguis@users.noreply.github.com>
2023-12-14 03:02:53 +0200

    editing path

--- app/src/main/java/com/example/securinzy/DBConnection.java
+++ app/src/main/java/com/example/securinzy/DBConnection.java
@@ -5,8 +5,7 @@ import java.sql.SQLException;
 
 public class DBConnection {
     //Christine's URL
-    private static final String DB_URL = "jdbc:ucanaccess://C://Users//kokig//OneDrive//Desktop//DBSecurinzy.accdb";
-
+    private static final String DB_URL = "jdbc:ucanaccess://C:\\Users\\kokig\\OneDrive\\Desktop\\DBSecurinzyyyy.accdb";
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(DB_URL);
     }
