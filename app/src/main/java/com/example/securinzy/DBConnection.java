5c000a04a84ed01ac11717050048c4f0afce1635
Mohamed Agamy <cds.mohamedahmed30407@alexu.edu.eg>
2023-12-18 01:28:35 +0200

    My final touch
    Agamy w Raga2ee byda7o

--- app/src/main/java/com/example/securinzy/DBConnection.java
+++ app/src/main/java/com/example/securinzy/DBConnection.java
@@ -2,10 +2,15 @@ package com.example.securinzy;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DBConnection {
-    //Christine's URL
-    private static final String DB_URL = "jdbc:ucanaccess://C:\\Users\\kokig\\OneDrive\\Desktop\\DBSecurinzyyyy.accdb";
+    private static final String DB_URL = "jdbc:ucanaccess://C://Users//Acer//Downloads//Desktop//DBSecurinzy.accdb";
+
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(DB_URL);
     }
